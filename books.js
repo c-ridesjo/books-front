6195ef1cc0d437a000c546d4ac7e1824fe2b5a80
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-03-10 11:01:16 +0100

    Add function printBooks

--- books.js
+++ books.js
@@ -14,3 +14,38 @@ function updateBookList() {
 
 updateBookList();
 
+function printBooks(books) {
+    bookList.innerHTML = "";
+
+    books.map(book => {
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        const borrowBook = document.createElement("button");   
+        borrowBook.id = book.id;
+        borrowBook.borrowed = book.borrowed;
+        button.innerText = "Mer info";
+        button.id = book.id;
+        button.title = book.title;
+        button.author = book.author;
+        button.available = book.borrowed;
+        button.pages = book.pages
+        li.id = book.id;
+        li.innerText = book.title;
+        bookList.appendChild(li);
+        bookList.appendChild(button);
+        bookList.appendChild(borrowBook);
+        
+        checkIfBookIsBorrowed(book);
+
+        button.addEventListener("click, showBookInfo");
+        borrowBook.addEventListener("click", changeToBorrowed);
+
+        if (book.borrowed) {
+            borrowBook.innerText = "Återlämna boken";
+        }
+        else {
+            borrowBook.innerText = "Låna boken";
+        }
+    })
+}
+
